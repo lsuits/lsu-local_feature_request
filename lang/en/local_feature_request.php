65e1beb3931c5b2f5af001d5580cbd58f0f3dfd8
Robert Russo <rrusso@lsu.edu>
2012-08-27 09:48:59 -0500

    Lang string change

--- lang/en/local_feature_request.php
+++ lang/en/local_feature_request.php
@@ -20,10 +20,10 @@ $string['disclaimer_help'] = 'If filled in, this disclaimer will be displayed on
 $string['success'] = 'Feature request successfully sent';
 $string['failure'] = 'Feature request could not be sent, please press send again or contact the Moodle Administrator.';
 
-$string['default_disclaimer'] = '<p>Items submitted via this form may be tracked by visiting the MDAC web site (<a href = "http://itsweb.lsu.edu/MDAC/index.html">http://itsweb.lsu.edu/MDAC/index.html</a>).</p>
-
+$string['default_disclaimer'] = '<p>
+    <strong>This form is for feature requests or suggested improvements to Moodle. If you have questions regarding the use of Moodle, please contact the Help Desk at 578-3375.</strong>
+</p>
+<p>Items submitted via this form may be tracked by visiting the MDAC web site (<a href = "http://itsweb.lsu.edu/MDAC/index.html">http://itsweb.lsu.edu/MDAC/index.html</a>).</p>
 <p>Submissions will be posted along with the name of the submitter.</p>
-
 <p>Should there be any questions or need for further clarification regarding the submission, you may be contacted by a Moodle administrator.</p>
-
 <p>Thank you for your ideas and suggestions.</p>';
